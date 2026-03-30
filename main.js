3a0f28c2794c81f03a9cfe221d823f1f29967935
Mateo BAL <mateo.bal345@gmail.com>
2026-03-30 08:39:25 +0200

    add nuke it function

--- main.js
+++ main.js
@@ -11,18 +11,30 @@ form.addEventListener("submit", (e) => {
 
     const card = document.createElement("div");
     card.classList.add("card");
-
+    const colors = ["#ff1493", "#00f632", "#00bfff"];
+    const value_color = colors[Math.floor(Math.random() * colors.length)];
+    console.log(value_color);
     card.innerHTML = `
       <div class="card-inner">
-        <div class="card-front">
+        <div class="card-front bg-[${value_color}]">
           <span class="wtf-text">WTF?</span>
           <span class="card-hint">CLICK 2 REVEAL</span>
         </div>
         <div class="card-back">
           <span class="task-text">${value}</span>
+          <button class="nuke-btn">Nuke it</button>
         </div>
       </div>
     `;
+
+    const nukeBtn = card.querySelector('.nuke-btn');
+    nukeBtn.addEventListener('click', (e) => {
+      e.stopPropagation();
+      card.classList.add('exploding');
+      setTimeout(() => {
+        card.remove();
+      }, 1000);
+    });
 
     card.addEventListener("click", () => {
       card.classList.toggle("flipped");
